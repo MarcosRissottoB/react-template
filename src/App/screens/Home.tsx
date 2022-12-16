848a204d70a2716fe5440ac3df90835b3c93f7fb
marcosirb <marcosrissotto@gmail.com>
2022-12-16 09:35:43 -0300

    Add basic layout in homescreen

--- src/App/screens/Home.tsx
+++ src/App/screens/Home.tsx
@@ -1,23 +1,92 @@
 import * as React from "react";
-import { Stack } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Flex, Grid, GridItem, Spacer, Stack } from "@chakra-ui/react";
 
 import { useCharacters } from "@/character/hooks";
-
+import Title from "@/App/components/title";
+import SearchBar from "@/App/components/searchBar";
 import CharacterList from "@/character/components/characterList";
 
 const HomeScreen: React.FC = () => {
   const [characters, secondCharacters, searchByName] = useCharacters()
-
+  const [pageNumber, setPageNumber] = React.useState(1)
+  const [secondCharacterPageNumber, setSecondCharacterPageNumber] = React.useState(1)
+  
   return (
-    <Stack direction='row'>
-      <Stack flex={1} spacing={2}>
-        <CharacterList characters={characters} searchByName={searchByName}/>
-      </Stack>
-      <Stack flex={1} spacing={2}>
-        <CharacterList characters={secondCharacters} searchByName={searchByName}/>
-         {/* TODO: Add EpisodesList component */}
-      </Stack>
-    </Stack>
+    <Grid
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(1, 1fr)'
+        gap={4}
+      > 
+        <GridItem rowSpan={1}>
+          <Grid
+            templateRows='repeat(1, 1fr)'
+            templateColumns='repeat(6, 1fr)'
+            gap={2}
+          >
+            <GridItem colSpan={3}>
+              <Box border='1px' borderColor='gray.200'>
+                <Stack flex={1} spacing={2}>
+                  <Stack direction='row'>
+                    <Flex minWidth='max-content' gap='0' direction='row' alignItems="center">
+                      <Title>Character #1</Title>
+                      <Spacer />
+                      <SearchBar searchByName={searchByName} pageNumber={pageNumber}/>
+                      <ButtonGroup >
+                        <Button onClick={() => setPageNumber((pageNumber > 0) ? pageNumber - 1: 0)} colorScheme='teal' variant='outline'>Previous</Button>
+                        <Button onClick={() => setPageNumber((characters && pageNumber < characters.info.pages) ? pageNumber + 1 : pageNumber)} colorScheme='teal' variant='outline'>Next</Button>
+                      </ButtonGroup>
+                    </Flex>
+                  </Stack>
+                  <CharacterList characters={characters} />
+                </Stack>
+              </Box>
+            </GridItem>
+            <GridItem colSpan={3}>
+              <Box border='1px' borderColor='gray.200'>
+                <Stack flex={1} spacing={2}>
+                  <Stack direction='row'>
+                    <Flex minWidth='max-content' gap='0' direction='row' alignItems="center">
+                      <Title>Character #2</Title>
+                      <Spacer />
+                      <SearchBar searchByName={searchByName} pageNumber={secondCharacterPageNumber} isSecondCharacter={true} />
+                      <ButtonGroup >
+                        <Button onClick={() => setSecondCharacterPageNumber(secondCharacterPageNumber - 1)} colorScheme='teal' variant='outline'>Previous</Button>
+                        <Button onClick={() => setSecondCharacterPageNumber((secondCharacters && secondCharacterPageNumber < secondCharacters.info.pages) ? secondCharacterPageNumber + 1 : secondCharacterPageNumber)} colorScheme='teal' variant='outline'>Next</Button>
+                      </ButtonGroup>
+                    </Flex>
+                  </Stack>
+                  <CharacterList characters={secondCharacters} />
+                </Stack>
+                </Box>
+            </GridItem>
+          </Grid>
+        </GridItem>
+        <GridItem rowSpan={1}>
+        <Grid
+          h='400px'
+          templateRows='repeat(1, 1fr)'
+          templateColumns='repeat(6, 1fr)'
+          gap={2}
+        >
+          <GridItem colSpan={2}>
+            <Box border='1px' borderColor='gray.200' height='500px'>
+              <Title>EpisodesList</Title>
+            </Box>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Box border='1px' borderColor='gray.200' height='500px'>
+              <Title>Share EpisodesList</Title>
+            </Box>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Box border='1px' borderColor='gray.200' height='500px'>
+            <Title>EpisodesList</Title>
+            </Box>
+          </GridItem>
+        </Grid>
+      </GridItem>
+    </Grid>
+    
   )
 }
 
--- src/character/components/characterList/CharacterList.tsx
+++ src/character/components/characterList/CharacterList.tsx
@@ -1,34 +1,16 @@
 
 import React from 'react'
-import { Button, ButtonGroup, Flex, SimpleGrid, Spacer, Stack, Text } from '@chakra-ui/react'
-
+import { Flex, SimpleGrid, Stack, Text } from '@chakra-ui/react'
 import { responseCharacter } from '@/character/types'
 import Card from '@/App/components/card'
-import Title from "@/App/components/title";
-import SearchBar from "@/App/components/searchBar";
 
 interface Props {
   characters: responseCharacter | undefined,
-  searchByName: (value: string, pageNumber: number, option?: boolean | undefined) => void
 }
 
-const CharacterList: React.FC<Props> = ({characters, searchByName}) => {
-  const [pageNumber, setPageNumber] = React.useState(1)
-
+const CharacterList: React.FC<Props> = ({characters}) => {
   return (
-    <Stack flex={1} spacing={2}>
-        <Stack direction='row'>
-          <Flex minWidth='max-content' gap='0' direction='row' alignItems="center">
-            <Title>Character #1</Title>
-            <Spacer />
-            <SearchBar searchByName={searchByName} pageNumber={pageNumber}/>
-            <ButtonGroup >
-              <Button onClick={() => setPageNumber(pageNumber > 0 ? pageNumber - 1: 0)} colorScheme='teal' variant='outline'>Previous</Button>
-              <Button onClick={() => setPageNumber(pageNumber + 1)} colorScheme='teal' variant='outline'>Next</Button>
-            </ButtonGroup>
-          </Flex>
-        </Stack>
-    <SimpleGrid columns={3} spacing={3}>
+    <SimpleGrid columns={3} spacing={3} height={400}>
       {!characters && 
         <Stack flex={1} spacing={2}>
           <Flex minWidth='max-content' direction='row' alignItems="center">
@@ -38,7 +20,6 @@ const CharacterList: React.FC<Props> = ({characters, searchByName}) => {
       {characters?.results.map((item) => 
         <Card key={item.id} item={item} />)}
     </SimpleGrid>
-    </Stack>
   )
 }
 
